0d11a21636a40139d1c9292d1ed0d0a632622e1c
Ankit Mordhwaj <mordhwaj.ankit@gmail.com>
2023-01-12 13:16:59 +0530

    added post api

--- drinks/views.py
+++ drinks/views.py
@@ -1,14 +1,24 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def DrinksList(request):
-    # get all the drinks
-    drinks = Drink.objects.all()
-    # serialize them, many=true to serial all the list
-    serializer = DrinkSerializer(drinks, many=True)
-    # return json
-    return JsonResponse({'drinks': serializer.data}, safe=False)
-    
+    if request.method == 'GET':
+        # get all the drinks
+        drinks = Drink.objects.all()
+        # serialize them, many=true to serial all the list
+        serializer = DrinkSerializer(drinks, many=True)
+        # return json
+        return JsonResponse({'drinks': serializer.data}, safe=False)
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
